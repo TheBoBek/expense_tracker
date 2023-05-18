7daec44c82285ba18675f4fb9fa6191c90a0a4db
TheBoBek <85736591+TheBoBek@users.noreply.github.com>
2023-05-18 10:01:59 +0200

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -9,21 +9,26 @@ class DataManager:
     def __init__(self):
         pass
 
-    def save(self, input1, input2, var, var2):
+    def save(self, input1, input2, var, var2, account_balance):
+        # Getting data from MainWindow inputs
         var_get = var.get()
         name_get = input1.get()
         value_get = input2.get()
         type_get = var2.get()
         now_str = str(datetime.now())
+        value_get = float(value_get[:-1])
+        if var_get == 2:
+            value_get *= -1
+            print(value_get)
         try:
-            value_get = float(value_get[:-1])
+            value_get = value_get
         except ValueError:
             messagebox.showwarning(title="Chyba", message="Zadajte správnu hodnotu.")
+        if account_balance + value_get < 0:
+            messagebox.showwarning(title="Chyba", message="Váš účet nemôže byť v zápornej hodnote")
+
         else:
-            if var_get == 2:
-                value_get *= -1
-
-
+            # New dict for saving data
             new_dict = {
                 now_str: {
                     "Transaction": var_get,
@@ -32,7 +37,6 @@ class DataManager:
                     "Type": type_get,
                 }
             }
-
             try:
                 with open("data.json", "r") as data_file:
                     # Reading old data
